4968f71ae8cb1f1752a70f1b1a7e3e9cc8514a85
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-07-12 11:42:26 +0300

    chore: styled header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import 'bulma/css/bulma.min.css';
+import './header.scss'
+import Cart from "../../img/icons8-cart-96.png"
 import {Navbar, Dropdown, Icon} from "react-bulma-components"
 function Header() {
   return (
     <div>
-      <Navbar color="black" style={{height:'110px'}}> 
-        <Navbar.Brand style={{color: '#fff'}}> React-Shop </Navbar.Brand>
+      <Navbar color="black" className='header'> 
+        <Navbar.Brand style={{color: '#fff'}} className='header_text'> React-Shop </Navbar.Brand>
+        <img src={Cart} alt="cart" />
       </Navbar>
     </div>
   )
