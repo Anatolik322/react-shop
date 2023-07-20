f69845dddd23e0a92024b465d0c37b24914dafa1
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-07-20 11:13:41 +0300

    feat: changed api

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -8,25 +8,29 @@ function ProductList() {
   //const { isSuccess, data} = useGetGoodsQuery()
   
   const [category, setCategory] = useState(null);
-  const {isSuccess, isLoading, data} = useGetByCategoryQuery(category);
- // s&& console.log(d )
-
+  const {isSuccess, isFetching, isLoading, data} = useGetByCategoryQuery(category);
+ isSuccess && console.log(data);
   return (
     <>
       <div className="filter">
           <h3> Category: </h3> 
           <Dropdown label="Category:" closeOnSelect={true} color={'dark'}icon={<Icon>V</Icon>} onChange={(e) => setCategory(e)}>
           <Dropdown.Item renderAs="a" value={null}>all</Dropdown.Item>
-            <Dropdown.Item renderAs="a" value="electronics">electronics</Dropdown.Item>
-            <Dropdown.Item renderAs="a" value="jewelery">jewelery</Dropdown.Item>
-            <Dropdown.Item renderAs="a" value="jewelery">jewelery</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="smartphones">smartphones</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="laptops">laptops</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="fragrances">fragrances</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="skincare">skincare</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="groceries">groceries</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="home-decoration">home-decoration</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="furniture">furniture</Dropdown.Item>
+            <Dropdown.Item renderAs="a" value="tops">tops</Dropdown.Item>
           </Dropdown>
         </div>
       <div className='product_list'>
-        {isLoading ? <h2>Loading...</h2>:
-        isSuccess && data.map(e => {
+        {isLoading || isFetching ? <h2>Loading...</h2>:
+        isSuccess && data.products.map(e => {
           return <Product 
-          img={e.image}
+          img={e.images[0]}
           title={e.title}
           price={e.price}
           descr={e.description}/>
--- src/services/shop.js
+++ src/services/shop.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 export const shopApi = createApi({
     reducerPath: 'shopApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://fakestoreapi.com'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://dummyjson.com'}),
     endpoints: builder => ({
         getGoods: builder.query({
             query: () => '/products',
