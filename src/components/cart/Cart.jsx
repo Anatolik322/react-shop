5240852c2c13e04f0b3a7f89aaf5b73f9c606e5c
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-07-21 10:14:18 +0300

    chore: created footer

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,24 +3,29 @@ import Header from '../header/Header'
 import useCart from '../../hooks/useCart';
 import Product from '../poroduct';
 import "../productList/ProductLIst.scss"
+import Footer from '../footer/Footer';
 function Cart() {
 
   const cart = useCart()
-  console.log(cart)
+  let totalPrice = 0;
   return (
     <div className='cart'>
       <Header></Header>
       <div className='product_list'>
         {cart.length > 0 && cart.map(e => {
+          totalPrice+=e.price
         return <Product 
           img={e.img}
           title={e.title}
           price={e.price}
           descr={e.descr}
           isInCart={true}
-          id={e.id}/>
+          id={e.id}
+          key={Math.random()}/>
         })}
       </div>
+      <h3> Total: {totalPrice}</h3>
+      <Footer></Footer>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Block, Columns } from 'react-bulma-components'
+import './footer.scss'
 
 function Footer() {
   return (
-    <div>
-      Footer
-    </div>
+   <Block className='footer'>
+      <Columns>
+        <Columns.Column className='footer_inner'>
+          <ul>
+              <h2>Contact us:</h2>
+              <li>
+                0 900 900 99
+              </li>
+              <li>
+                example@gmail.com
+              </li>
+          </ul>
+        </Columns.Column>
+        <Columns.Column style={{color: '#fff', textAlign: 'left'}}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, ab,
+          saepe facilis dicta sed quaerat delectus quas vitae sequi modi sunt nam quasi,
+          incidunt itaque. Consequatur aliquam dicta ducimus omnis.
+        </Columns.Column>
+      </Columns>
+   </Block>
   )
 }
 
