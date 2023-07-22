c83c1a6e7e688cc08585f54e03af0b1ccd9f2616
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-07-22 15:07:51 +0300

    fix: fixed bug with removing from cart

--- src/services/cartSlice.js
+++ src/services/cartSlice.js
@@ -9,20 +9,9 @@ export const cartSlice = createSlice({
     initialState,
     reducers:{
         addToCart: (state, action) => {
-            //const index = state.cartArr.findIndex(item => item.id === action.payload.id);
-            // state.cartArr.map(e => {
-            //     console.log(e)
-            // })
-            //e.id !== action.payload
+           
             const idArray = state.cartArr.map(item => item.id);
-            // state.cartArr.length > 0 ? 
-            // state.cartArr = state.cartArr.map(e => {
-            //     console.log('cart', e.id, 'payload', action.payload.id)
-            //     if(e.id !== action.payload.id){
-            //        console.log('first time')
-            //        state.cartArr.push(action.payload)
-            //     }else{ e.quantity+= 1; console.log('incluse')}
-            // }): state.cartArr.push(action.payload)
+
             if(idArray.includes(action.payload.id)){
                 state.cartArr.forEach(item => {
                     if (item.id === action.payload.id) {
@@ -34,11 +23,22 @@ export const cartSlice = createSlice({
             }
         },
         removeFromCart: (state, action) => {
-            const index = state.cartArr.findIndex(item => item.id === action.payload);
-            console.log(action.payload)
-            state.cartArr = state.cartArr.filter(e => {
-                return e.id !== action.payload
-            })
+            const idArray = state.cartArr.map(item => item.id);
+
+            if(idArray.includes(action.payload)){
+                state.cartArr.forEach(item => {
+                    if (item.id === action.payload && item.quantity > 1 ) {
+                      item.quantity-=1 
+                    }else{
+                        state.cartArr = state.cartArr.filter(e => {
+                            if (e.id === action.payload ) {
+                                e.quantity-=1 
+                            }
+                            return e.id !== action.payload
+                        })
+                    }
+                });
+            }
         },
         clearState: (state, action) => {
             state.cartArr = []
