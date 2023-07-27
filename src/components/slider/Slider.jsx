26a78ecfcdfe704880edbc222151397d7d1fe150
Anatolik322 <anatoliysynecun2016@gmail.com>
2023-07-27 15:28:45 +0300

    build: deployed app

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -14,11 +14,13 @@ function Slider() {
 
   return (
     <div id='slider'>
-         <Zoom scale={0.1} id="slider_comp">
+         {/* <Zoom scale={0.1} id="slider_comp">
           {
             images.map((each, index) => <img key={index} style={{width: "100%"}} src={each} alt='promo baner'/>)
           }
-        </Zoom>
+        </Zoom> */
+        }
+        <img src={s2} alt="baner" />
     </div>
   )
 }
